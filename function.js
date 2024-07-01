c02244528db8a2081d548191ed8c8bb4c145c9e9
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2024-07-01 23:37:31 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+// Function Declaration
+function add (a , b) {
+    return a + b;
+};
+console.log(add(10, 20));
+
+// Function Expression
+const multy = function (a,b) {
+    return a * b;
+}
+console.log(multy(2,4));
